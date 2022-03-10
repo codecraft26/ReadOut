e61a7cf28c4da44cc0c6ba2c6845b88683570442
codecraft26 <codescraft26@gmail.com>
2022-03-11 01:00:45 +0530

    Initial commit

---
+++ app/src/main/java/dev/codecraft/readout/MainActivity.kt
@@ -0,0 +1,15 @@
+package dev.codecraft.readout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.codecraft.readout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
